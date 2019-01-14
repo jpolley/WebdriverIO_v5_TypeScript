dec621c79968f905a16a9ba7118fb811fc9f236d
Jeremy Polley <jeremypolley@gmail.com>
2019-01-14 17:55:28 -0500

    specify spec reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -104,7 +104,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    // reporters: ['dot'],
+    reporters: ['spec'],
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
