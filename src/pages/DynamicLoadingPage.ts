96d93cc62adadcce6d9dd787a92d9405491a8ea7
Jeremy Polley <jeremypolley@gmail.com>
2019-01-14 17:54:30 -0500

    add dynamic loading example

---
+++ src/pages/DynamicLoadingPage.ts
@@ -0,0 +1,34 @@
+import BasePage from 'src/pages/BasePage';
+
+class DynamicLoadingPage extends BasePage {
+    get open() {
+        return browser.url('/dynamic_loading');
+    }
+
+    get elementExistsButIsHidden() {
+        return $('[href="/dynamic_loading/1"]').click();
+    }
+
+    get elementIsRenderedAfterLoading() {
+        return $('[href="/dynamic_loading/2"]').click();
+    }
+
+    get start() {
+        return $('#start > button').click();
+    }
+
+    get finishElement() {
+        return $('#finish > h4');
+    }
+
+    get finishText() {
+        return this.finishElement.getText()
+    }
+
+    waitUntilElementIsDisplayed() {
+        browser.waitUntil(() => {
+            return this.finishElement.isDisplayed()
+        })
+    }
+}
+export default new DynamicLoadingPage();
---
+++ test/dynamicLoading.spec.ts
@@ -0,0 +1,22 @@
+import {expect} from 'chai';
+import DynamicLoadingPage from 'src/pages/DynamicLoadingPage';
+
+describe('Dynamic loading', () => {
+    it('is no problem with waitUntil', () => {
+        DynamicLoadingPage.open;
+        DynamicLoadingPage.elementExistsButIsHidden;
+        DynamicLoadingPage.start;
+        DynamicLoadingPage.waitUntilElementIsDisplayed();
+
+        expect(DynamicLoadingPage.finishText).to.eq('Hello World!');
+    });
+
+    it('even when element is rendered after loading', () => {
+        DynamicLoadingPage.open;
+        DynamicLoadingPage.elementIsRenderedAfterLoading;
+        DynamicLoadingPage.start;
+        DynamicLoadingPage.waitUntilElementIsDisplayed();
+
+        expect(DynamicLoadingPage.finishText).to.eq('Hello World!');
+    })
+});
