f5999a7bbe9d27c974bdd33c7ed808f143eac2f5
Jeremy Polley <jeremypolley@gmail.com>
2019-01-15 05:53:15 -0500

    lint

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -5,12 +5,14 @@ describe('Login page', () => {
     it('displays message with invalid credentials', () => {
         LoginPage.open;
         LoginPage.loginWithCredentials('foo', 'bar');
+
         expect(LoginPage.flash).to.include('Your username is invalid!');
     });
 
     it('should allow access with correct credentials', () => {
         LoginPage.open;
         LoginPage.loginWithCredentials('tomsmith', 'SuperSecretPassword!');
+
         expect(LoginPage.flash).to.include('You logged into a secure area!');
     });
 });
