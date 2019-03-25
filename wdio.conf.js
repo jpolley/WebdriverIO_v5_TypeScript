7a3e9d7cbafbf6f0553b9784016626b5e6a3590f
Jeremy Polley <jeremypolley@gmail.com>
2019-03-25 19:12:15 -0400

    try headless option

--- wdio.conf.js
+++ wdio.conf.js
@@ -54,7 +54,10 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 1,
         //
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu'],
+        }
     }],
     //
     // ===================
